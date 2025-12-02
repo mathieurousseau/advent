7425c516d0c8718139627c0ae4e67bec183e09b0
mathieurousseaau <mathieu.rousseau.31@gmail.com>
2025-12-02 10:06:55 -0500

    day 2

--- lib/2025/days/day_02_1.ex
+++ lib/2025/days/day_02_1.ex
@@ -1,13 +1,46 @@
 defmodule Aoc2025.Day02One do
-  @expected nil
+  @expected 1_227_775_554
   def run(input) do
     {do_run(input), @expected}
   end
 
   defp do_run(input) do
     parse(input)
+    |> Enum.reduce(0, fn range, acc ->
+      Enum.reduce(range, acc, fn n, acc ->
+        # check is length of n is even
+        if Integer.digits(n) |> length() |> rem(2) == 0 do
+          if symetrical(Integer.to_string(n)) do
+            acc + n
+          else
+            acc
+          end
+        else
+          acc
+        end
+      end)
+    end)
   end
 
-  defp parse(_input) do
+  defp symetrical(str) do
+    len = String.length(str)
+    half = div(len, 2)
+    first_half = String.slice(str, 0, half) |> String.to_integer()
+    second_half = String.slice(str, half, half) |> String.to_integer()
+
+    first_half == second_half
+  end
+
+  defp parse(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> hd()
+    |> String.split(",")
+    |> Enum.map(&parse_range/1)
+  end
+
+  defp parse_range(range) do
+    [start, stop] = String.split(range, "-")
+    String.to_integer(start)..String.to_integer(stop)
   end
 end
--- lib/2025/days/day_02_2.ex
+++ lib/2025/days/day_02_2.ex
@@ -1,13 +1,48 @@
 defmodule Aoc2025.Day02Two do
-  @expected nil
+  @expected 4_174_379_265
   def run(input) do
     {do_run(input), @expected}
   end
 
   defp do_run(input) do
     parse(input)
+
+    # [95..115]
+    |> Enum.reduce(0, fn range, acc ->
+      Enum.reduce(range, acc, fn n, acc ->
+        # check is length of n is even
+        if repeated(Integer.to_string(n)) do
+          acc + n
+        else
+          acc
+        end
+      end)
+    end)
   end
 
-  defp parse(_input) do
+  defp repeated(str) do
+    len = String.length(str)
+
+    1..len
+    |> Enum.any?(fn size ->
+      chunks =
+        String.graphemes(str) |> Enum.chunk_every(size)
+
+      length(chunks) > 1 and
+        MapSet.new(chunks) |> MapSet.size() == 1
+    end)
+  end
+
+  defp parse(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> hd()
+    |> String.split(",")
+    |> Enum.map(&parse_range/1)
+  end
+
+  defp parse_range(range) do
+    [start, stop] = String.split(range, "-")
+    String.to_integer(start)..String.to_integer(stop)
   end
 end
